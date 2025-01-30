97784221e943a2452620458c1bc8b69cd5a328f6
olveve <olve.vassgard.eid@gmail.com>
2025-01-30 10:18:14 +0100

    Endra størrelse på bilder og generelt

--- backgrounds.py
+++ backgrounds.py
@@ -12,23 +12,28 @@ mountain_back = pg.image.load("assets/Background/Mountain_Back.png").convert_alp
 clouds = pg.image.load("assets/Background/Clouds.png").convert_alpha()
 sky = pg.image.load("assets/Background/Sky.png").convert_alpha()
 
-gras = pg.transform.scale(gras, (1024, 704))
-ground = pg.transform.scale(ground, (1024, 704))
-trees = pg.transform.scale(trees, (1024, 704))
-backgroundtrees = pg.transform.scale(backgroundtrees, (1024, 704))
-mountain_front = pg.transform.scale(mountain_front, (1024, 704))
-mountain_middle = pg.transform.scale(mountain_middle, (1024, 704))
-mountain_back = pg.transform.scale(mountain_back, (1024, 704))
-clouds = pg.transform.scale(clouds, (1024, 704))
-sky = pg.transform.scale(sky, (1024, 704))
+gras = pg.transform.scale(gras, (1059, 504))
+ground = pg.transform.scale(ground, (1059, 504))
+trees = pg.transform.scale(trees, (1059, 504))
+backgroundtrees = pg.transform.scale(backgroundtrees, (1059, 504))
+mountain_front = pg.transform.scale(mountain_front, (1059, 504))
+mountain_middle = pg.transform.scale(mountain_middle, (1059, 504))
+mountain_back = pg.transform.scale(mountain_back, (1059, 504))
+clouds = pg.transform.scale(clouds, (1059, 504))
+sky = pg.transform.scale(sky, (1059, 504))
 
 
 
 def draw_bg(screen):
     # screen.fill(BG)
     screen.blit(sky, (0,0))
-    screen.blit(clouds, (0, HEIGHT - clouds.get_height() - 300))
-    screen.blit(mountain_back, (0, HEIGHT - mountain_back.get_height() - 300))
-    screen.blit(mountain_middle, (0, HEIGHT - mountain_middle.get_height() - 300))
+    screen.blit(clouds, (0, HEIGHT - clouds.get_height() - 150))
+    screen.blit(mountain_back, (0, HEIGHT - mountain_back.get_height()))
+    screen.blit(mountain_middle, (0, HEIGHT - mountain_middle.get_height()))
+    screen.blit(mountain_front, (0, HEIGHT - mountain_front.get_height()))
+    screen.blit(backgroundtrees, (0, HEIGHT - backgroundtrees.get_height()))
+    screen.blit(trees, (0, HEIGHT - trees.get_height()))
+    screen.blit(ground, (0, HEIGHT - ground.get_height()))
+    screen.blit(gras, (0, HEIGHT - gras.get_height()))
 
 
