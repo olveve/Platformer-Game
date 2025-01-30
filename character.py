fae0b50719085d0509348c1b1c60384e4778ab36
OlaLunde <ola.berghlunde@gmail.com>
2025-01-30 10:25:54 +0100

    fiksa sånn at firkanten kan bevege seg og står riktig plassert

--- character.py
+++ character.py
@@ -8,7 +8,7 @@ class Samurai:
         self. y = y
         self.vx = 5
         self.vy = 0
-        self.jump = -5
+        self.jump = -22
         self.width = 64
         self.height = 64
         self.color = (255, 0, 0)
@@ -20,21 +20,21 @@ class Samurai:
         self.vy += GRAVITY
         self.y += self.vy
 
-        #if not self.dead and self.y > HEIGHT - self.height:
-        self.y = HEIGHT - self.height
-        self.vy = 0
+        if self.y > HEIGHT - (self.height + 67):
+            self.y = HEIGHT - (self.height + 67)
+            self.vy = 0
 
-        keys_pressed = pg.key.get_pressed()
-        if keys_pressed[pg.K_LEFT] and self.x > 0:
-            self.x -= self.vx
-            #self.frame_counter += 1
-            #self.image = #mario_sprites_left[(self.frame_counter // 3) % len(mario_sprites_left)]
-        if keys_pressed[pg.K_RIGHT] and self.x < WIDTH - self.width:
-            self.x += self.vx
-            #self.frame_counter += 1 
-            # (self.frame_counter + 1) % len(mario_sprites)
-            #self.image = mario_sprites[ (self.frame_counter // 3) % len(mario_sprites) ]
-        if keys_pressed[pg.K_UP]:
-            if self.vy == 0:
-                self.vy = self.jump
-        
+            keys_pressed = pg.key.get_pressed()
+            if keys_pressed[pg.K_LEFT] and self.x > 0:
+                self.x -= self.vx
+                #self.frame_counter += 1
+                #self.image = #mario_sprites_left[(self.frame_counter // 3) % len(mario_sprites_left)]
+            if keys_pressed[pg.K_RIGHT] and self.x < WIDTH - self.width:
+                self.x += self.vx
+                #self.frame_counter += 1 
+                # (self.frame_counter + 1) % len(mario_sprites)
+                #self.image = mario_sprites[ (self.frame_counter // 3) % len(mario_sprites) ]
+            if keys_pressed[pg.K_UP]:
+                if self.vy == 0:
+                    self.vy = self.jump
+            
