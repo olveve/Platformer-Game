6c22e238e8efddc49c0aedac682a330fee9c5ebe
OlaLunde <ola.berghlunde@gmail.com>
2025-02-04 15:27:59 +0100

    Update character.py

--- character.py
+++ character.py
@@ -2,11 +2,10 @@ import pygame as pg
 from settings import *
 from assets import *
 
-class Samurai:
-    def __init__(self, x, y, char_type, vx):
+class Character:
+    def __init__(self, x, y, vx):
         self.x = x
         self. y = y
-        self.char_type = char_type
         self.vx = vx
         self.vy = 0
         self.jump = -22
@@ -17,6 +16,8 @@ class Samurai:
 
     def draw(self, screen):
         pg.draw.rect(screen, self.color, (self.x, self.y, self.width, self.height))
+        # TODO: Draw the image of the character using blit
+        # screen.blit(self.image, (self.x, self.y))
 
     def movement(self):
         screen_scroll = 0
@@ -27,31 +28,51 @@ class Samurai:
             self.y = HEIGHT - (self.height + 67)
             self.vy = 0
 
-        if self.char_type == "samurai":
-            keys_pressed = pg.key.get_pressed()
-            if keys_pressed[pg.K_LEFT] and self.x > 0:
-                self.x -= self.vx
-                
-            if keys_pressed[pg.K_RIGHT] and self.x < WIDTH - self.width:
-                self.x += self.vx
-                
-            if keys_pressed[pg.K_UP]:
-                if self.y == HEIGHT - (self.height + 67):
-                    self.vy = self.jump
 
-        if self.char_type == "ai":
-            distance_to_person = abs(self.x - person.x)
-            if distance_to_person <= 100:
-                self.following = True
-            if self.following:
-                if self.x < person.x:
-                    self.x += self.vx
-                if self.x > person.x:
-                    self.x -= self.vx
-        
         # Oppdatere screen_scroll basert på x-posisjonen til spilleren
         # if self.char_type == "samurai":
 
 
-person = Samurai(100, 100, "samurai", 10)
-enemy = Samurai(800, 100, "ai", 2)
+
+class Samurai(Character):
+    def __init__(self, x, y, vx):
+        super().__init__(x, y, vx)
+        self.color = (0, 255, 0)
+        # TODO: Set self.image to the image of the samurai
+
+
+    def movement(self):
+        super().movement()
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[pg.K_LEFT] and self.x > 0:
+            self.x -= self.vx
+            
+        if keys_pressed[pg.K_RIGHT] and self.x < WIDTH - self.width:
+            self.x += self.vx
+            
+        if keys_pressed[pg.K_UP]:
+            if self.y == HEIGHT - (self.height + 67):
+                self.vy = self.jump
+
+
+
+
+class Enemy(Character):
+    def __init__(self, x, y, vx):
+        super().__init__(x, y, vx)
+        # TODO: Set self.image to the image of the enemy
+
+    def movement(self):
+        super().movement()
+        distance_to_person = abs(self.x - person.x)
+        if distance_to_person <= 100:
+            self.following = True
+        if self.following:
+            if self.x < person.x:
+                self.x += self.vx
+            if self.x > person.x:
+                self.x -= self.vx
+
+
+person = Samurai(100, 100, 10)
+enemy = Enemy(800, 100, 2)
