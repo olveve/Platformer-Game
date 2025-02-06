65a9a84ac05656d9fe11a449056a16ab90d50241
OlaLunde <ola.berghlunde@gmail.com>
2025-02-06 08:48:01 +0100

    ai følger nå etter person selv når skjermen scroller, men ikke med riktig hastighet

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ while running:
     draw_bg(screen, bg_scroll)
     person.movement()
     person.draw(screen)
-    enemy.movement(is_scrolling, -scroll if is_scrolling else 0)
+    enemy.movement(is_scrolling, -person.vx if not is_scrolling else 0)
     enemy.draw(screen)
     
     pg.display.update()
